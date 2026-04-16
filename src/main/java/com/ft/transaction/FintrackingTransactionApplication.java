bd8a9926560855b11cc96ac230aa1909a77c0458
jae9380 <ljy5314@gmail.com>
2026-04-16 17:40:00 +0900

    Fintreacking Transaction Server

---
+++ src/main/java/com/ft/transaction/FintrackingTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.ft.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackingTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintrackingTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ft/transaction/FintrackingTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackingTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
