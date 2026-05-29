df28bdf947766a9b9217b6833d5af125dd731cf3
jae9380 <ljy5314@gmail.com>
2026-05-29 16:21:37 +0900

    chore: add @EnableJpaAuditing to all service applications

--- src/main/java/com/ft/transaction/FintrackingTransactionApplication.java
+++ src/main/java/com/ft/transaction/FintrackingTransactionApplication.java
@@ -2,7 +2,9 @@ package com.ft.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class FintrackingTransactionApplication {
 
